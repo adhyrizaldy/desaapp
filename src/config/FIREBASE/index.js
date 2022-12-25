aa5d7ea0e58413c6e6b9f8052f9f81e575174c2d
adhyrizaldy <adhyc4n@gmail.com>
2022-12-26 04:01:32 +0800

    Commit ke dua

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase';
+
+// firebase.initializeApp({
+//   apiKey: "",
+//   authDomain: "",
+//   databaseURL: "",
+//   projectId: "",                                                                           
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+// });
+firebase.initializeApp({
+  apiKey: "AIzaSyC5tZfoqZM_Lbu3owlNgozSUU5t9oUVAG0",
+  authDomain: "desaapp-v11.firebaseapp.com",
+  projectId: "desaapp-v11",
+  storageBucket: "desaapp-v11.appspot.com",
+  messagingSenderId: "31731289730",
+  appId: "1:31731289730:web:f822954e39ad8575caae9b",
+  measurementId: "G-E7YYY6QYYG"
+});     
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
